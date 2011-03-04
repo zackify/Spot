81c52c4e68794f3fa79304ad83339b812104f9cd
Zach <me@zackify.net>
2011-03-04 15:55:21 -0800

    Fixed a security issue with mysql injections

--- Spot/save.php
+++ Spot/save.php
@@ -1,7 +1,7 @@
 <?php
 require('connect.php');
-$title = $_POST['title'];
-$text =  $_POST['text'];
+$title = mysql_real_escape_string($_POST['title']);
+$text =  mysql_real_escape_string($_POST['text']);
 mysql_query("UPDATE content SET text='$text' WHERE title='$title'") or die(mysql_error());
 echo $text;
 ?>
